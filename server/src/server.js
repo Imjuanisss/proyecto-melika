ac3f9124fe77e5d39ad740aebb3e571a5dd628ef
Juaniss <Olartemejiajuanesteban@gmail.com>
2026-06-16 19:09:11 -0500

    preparando deploy

--- server/src/server.js
+++ server/src/server.js
@@ -11,7 +11,6 @@ const medicamentosRoutes  = require('./routes/medicamentosRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
-
 app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 
